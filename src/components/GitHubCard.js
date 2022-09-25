cf8f71bbb977dbb7f9630cc294b3f314fae73279
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-25 09:19:45 -0400

    added GitHubCard stub and profile jpeg

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import ProfilePic from './v_castaneda.jpeg'
+
+function GitHubCard() {
+    return (
+        <div>
+        Pic Stub
+        </div>
+    )
+}
+
+export default GitHubCard
