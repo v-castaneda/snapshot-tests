387f858d8e8bea8da5e5540af7ae11720a087acc
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-25 10:08:37 -0400

    removing borders'

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -5,15 +5,15 @@ import ProfilePic from './v_castaneda.jpeg'
 
 function GitHubCard() {
     return (
-        <div className='card'>
-            <Card style={{ width: '18rem'}}>
+        <div className='card' class='card border-0'>
+            <Card style={{ width: '18rem'}} class='card border-0'>
                 <Card.Img variant='top' src={ProfilePic} />
                 <Card.Body>
                     <Card.Title>Victor Castañeda</Card.Title>
                     <Card.Text>
                     I am budding code developer who likes 
-                    to travel, workout, and woodworking. I live 
-                    in NJ with my wife, Jenally, and three kids, 
+                    to travel, workout, and dabble in woodworking. I 
+                    live in NJ with my wife, Jenally, and three kids, 
                     Noah, Leah, and Joel.
                     </Card.Text>
                     <Button 
